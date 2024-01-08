fe8870a13e7bc2df5a7ad5319679e0ae325bb6ba
rokiboxofficial <84924680+rokiboxofficial@users.noreply.github.com>
2024-01-08 20:45:22 +0300

    Add Timers (#7)
    * Add Timers
    
    * Fix Elapsed Event For ChessTimer

---
+++ src/ChessChaos.Domain/Timers/IntervalTimer.cs
@@ -0,0 +1,30 @@
+﻿namespace ChessChaos.Domain.Timers;
+
+public class IntervalTimer : IIntervalTimer, IDisposable
+{
+	public event Action Elapsed = null!;
+	private readonly System.Timers.Timer _internalTimer;
+
+	public IntervalTimer(int interval)
+	{
+		_internalTimer = new System.Timers.Timer(interval);
+		_internalTimer.Elapsed += (_, _) => Elapsed?.Invoke();
+	}
+
+	public int Interval
+	{
+		get => (int)_internalTimer.Interval;
+		set => _internalTimer.Interval = value;
+	}
+
+	public bool Enabled => _internalTimer.Enabled;
+
+	public void Start()
+		=> _internalTimer.Start();
+
+	public void Stop()
+		=> _internalTimer.Stop();
+
+	public void Dispose()
+		=> _internalTimer.Dispose();
+}
---
+++ src/ChessChaos.DomainTests/ChessTimerTests/Utils.cs
@@ -0,0 +1,67 @@
+﻿using ChessChaos.Domain.Timers;
+using Moq;
+using ChessChaos.Domain.Abstractions;
+
+namespace ChessChaos.DomainTests.ChessTimerTests;
+
+public static class Utils
+{
+	public const int MillisecondsInSecond = 1000;
+
+	public static ChessTimer GetChessTimer(int initialTimeInMilliseconds)
+		=> GetChessTimer(() => DateTime.MinValue, initialTimeInMilliseconds);
+
+	public static (ChessTimer, IIntervalTimer) GetChessTimerAndIntervalTimer(int initialTimeInMilliseconds)
+		=> GetChessTimerAndIntervalTimer(() => DateTime.MinValue, initialTimeInMilliseconds);
+
+	public static (ChessTimer chessTimer, IIntervalTimer intervalTimer, Mock<IIntervalTimer> intervalTimerMock) GetChessTimerAndIntervalTimerAndMock(int initialTimeInMilliseconds)
+		=> GetChessTimerAndIntervalTimerAndMock(() => DateTime.MinValue, initialTimeInMilliseconds);
+
+	public static ChessTimer GetChessTimer(Func<DateTime> fakeDateTimeProvider, int initialTimeInMilliseconds)
+	{
+		var (chessTimer, _) = GetChessTimerAndIntervalTimer(fakeDateTimeProvider, initialTimeInMilliseconds);
+
+		return chessTimer;
+	}
+
+	public static (ChessTimer chessTimer, IIntervalTimer intervalTimer) GetChessTimerAndIntervalTimer(Func<DateTime> fakeDateTimeProvider, int initialTimeInMilliseconds)
+	{
+		var (chessTimer, intervalTimer, _) = GetChessTimerAndIntervalTimerAndMock(fakeDateTimeProvider, initialTimeInMilliseconds);
+
+		return (chessTimer, intervalTimer);
+	}
+
+	public static (ChessTimer chessTimer, IIntervalTimer intervalTimer, Mock<IIntervalTimer> intervalTimerMock) GetChessTimerAndIntervalTimerAndMock(Func<DateTime> fakeDateTimeProvider, int initialTimeInMilliseconds)
+	{
+		var dateTimeProvider = GetDateTimeNowProvider(fakeDateTimeProvider);
+		var (intervalTimer, intervalTimerMock) = GetIntervalTimerAndIntervalTimerMock();
+		var chessTimer = new ChessTimer(intervalTimer, dateTimeProvider, initialTimeInMilliseconds);
+
+		return (chessTimer, intervalTimer, intervalTimerMock);
+	}
+
+	private static IDateTimeNowProvider GetDateTimeNowProvider(Func<DateTime> fakeDateTimeProvider)
+	{
+		var dateTimeNowProviderMock = new Mock<IDateTimeNowProvider>();
+		dateTimeNowProviderMock
+			.Setup(dateTimeProvider => dateTimeProvider.Now)
+			.Returns(fakeDateTimeProvider);
+
+		var dateTimeProvider = dateTimeNowProviderMock.Object;
+
+		return dateTimeProvider;
+	}
+
+	private static (IIntervalTimer intervalTimer, Mock<IIntervalTimer> intervalTimerMock) GetIntervalTimerAndIntervalTimerMock()
+	{
+		var enabled = false;
+		var intervalTimerMock = new Mock<IIntervalTimer>();
+		intervalTimerMock.SetupProperty(intervalTimer => intervalTimer.Interval);
+		intervalTimerMock.Setup(intervalTimer => intervalTimer.Start()).Callback(() => enabled = true);
+		intervalTimerMock.Setup(intervalTimer => intervalTimer.Stop()).Callback(() => enabled = false);
+		intervalTimerMock.SetupGet(intervalTimer => intervalTimer.Enabled).Returns(() => enabled);
+		var intervalTimer = intervalTimerMock.Object;
+
+		return (intervalTimer, intervalTimerMock);
+	}
+}
