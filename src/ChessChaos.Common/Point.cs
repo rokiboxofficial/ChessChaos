8c8b77f3934cfd5948600607a6a990166f28a3a3
rokiboxofficial <rokiboxofficial@gmail.com>
2024-01-21 15:50:01 +0300

    Fix point

---
+++ src/ChessChaos.Common/Point.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ChessChaos.Common;
+
+public readonly struct Point
+{
+	public Point(int x, int y)
+	{
+		X = x;
+		Y = y;
+	}
+
+	public int X { get; }
+	public int Y { get; }
+
+	public override int GetHashCode()
+		=> HashCode.Combine(X, Y);
+
+	public override bool Equals([NotNullWhen(true)] object? obj)
+		=> obj is Point && Equals((Point)obj);
+
+	public bool Equals(Point other)
+		=> this == other;
+
+	public static bool operator ==(Point p1, Point p2)
+		=> p1.X == p2.X && p1.Y == p2.Y;
+
+	public static bool operator !=(Point p1, Point p2)
+		=> !(p1 == p2);
+
+	public Point Offset(Point offset)
+		=> new Point(offset.X + X, offset.Y + Y);
+}
