669f0bfb903b70065cd4e6b4a42895489d498a2b
Kirill <sosisks888@mail.ru>
2024-01-23 18:47:07 +0300

    fix formatting errors

--- src/ChessChaos.Common/Point.cs
+++ src/ChessChaos.Common/Point.cs
@@ -4,33 +4,33 @@ namespace ChessChaos.Common;
 
 public readonly struct Point
 {
-  public Point(int x, int y)
-  {
-    X = x;
-    Y = y;
-  }
+	public Point(int x, int y)
+	{
+		X = x;
+		Y = y;
+	}
 
-  public int X { get; }
-  public int Y { get; }
+	public int X { get; }
+	public int Y { get; }
 
-  public override int GetHashCode()
-    => HashCode.Combine(X, Y);
+	public override int GetHashCode()
+	  => HashCode.Combine(X, Y);
 
-  public override bool Equals([NotNullWhen(true)] object? obj)
-    => obj is Point && Equals((Point)obj);
+	public override bool Equals([NotNullWhen(true)] object? obj)
+	  => obj is Point && Equals((Point)obj);
 
-  public bool Equals(Point other)
-    => this == other;
+	public bool Equals(Point other)
+	  => this == other;
 
-  public static bool operator ==(Point p1, Point p2)
-    => p1.X == p2.X && p1.Y == p2.Y;
+	public static bool operator ==(Point p1, Point p2)
+	  => p1.X == p2.X && p1.Y == p2.Y;
 
-  public static bool operator !=(Point p1, Point p2)
-    => !(p1 == p2);
+	public static bool operator !=(Point p1, Point p2)
+	  => !(p1 == p2);
 
-  public Point Offset(Point offset)
-    => new Point(offset.X + X, offset.Y + Y);
+	public Point Offset(Point offset)
+	  => new Point(offset.X + X, offset.Y + Y);
 
-  public static Point operator -(Point p1, Point p2)
-    => new Point(p1.X - p2.X, p1.Y - p2.Y);
+	public static Point operator -(Point p1, Point p2)
+	  => new Point(p1.X - p2.X, p1.Y - p2.Y);
 }
--- src/ChessChaos.CommonTests/PointTests.cs
+++ src/ChessChaos.CommonTests/PointTests.cs
@@ -6,272 +6,272 @@ namespace ChessChaos.CommonTests;
 [TestClass]
 public class PointTests
 {
-  [TestMethod]
-  public void WhenCallingDefaultConstructor_ThenXAndYShouldBe0()
-  {
-    // Act.
-    var point = new Point();
-
-    // Assert.
-    point.X.Should().Be(0);
-    point.Y.Should().Be(0);
-  }
-
-  [TestMethod]
-  public void WhenCallingConstructorWithNonZeroNumbers_ThenXAndYShouldBeCorrect()
-  {
-    // Act.
-    var point = new Point(4, 6);
-
-    // Assert.
-    point.X.Should().Be(4);
-    point.Y.Should().Be(6);
-  }
-
-  [TestMethod]
-  public void WhenGettingHashCodes_AndPointsAreSame_ThenHashCodesShouldBeSame()
-  {
-    // Arrange.
-    var (firstPoint, secondPoint) = GetSamePoints();
-
-    // Act.
-    var firstPointHashCode = firstPoint.GetHashCode();
-    var secondPointHashCode = secondPoint.GetHashCode();
-
-    // Assert.
-    firstPointHashCode.Should().Be(secondPointHashCode);
-  }
-
-  [TestMethod]
-  public void WhenEquallingByEqualsMethod_AndPointsAreSame_ThenPointsShouldBeEqual()
-  {
-    // Arrange.
-    var (firstPoint, secondPoint) = GetSamePoints();
-
-    // Act.
-    var isPointsAreEqual = firstPoint.Equals(secondPoint);
-
-    // Assert.
-    isPointsAreEqual.Should().BeTrue();
-  }
-
-  [TestMethod]
-  public void WhenEquallingByEqualsMethod_AndPointsAreDifferent_ThenPointsShouldBeNotEqual()
-  {
-    // Arrange.
-    var (firstPoint, secondPoint) = GetDifferentPoints();
-
-    // Act.
-    var isPointsAreEqual = firstPoint.Equals(secondPoint);
-
-    // Assert.
-    isPointsAreEqual.Should().BeFalse();
-  }
-
-  [TestMethod]
-  public void WhenEquallingByEqualsMethod_AndSecondPointIsNull_ThenPointsShouldBeNotEqual()
-  {
-    // Arrange.
-    var firstPoint = new Point(1, 2);
-
-    // Act.
-    var isPointsAreEqual = firstPoint.Equals(null);
-
-    // Assert.
-    isPointsAreEqual.Should().BeFalse();
-  }
-
-  [TestMethod]
-  public void WhenEquallingByEqualsMethod_AndSecondPointIsInteger_ThenPointsShouldBeNotEqual()
-  {
-    // Arrange.
-    var firstPoint = new Point(1, 2);
-
-    // Act.
-    var isPointsAreEqual = firstPoint.Equals(9);
-
-    // Assert.
-    isPointsAreEqual.Should().BeFalse();
-  }
-
-  [TestMethod]
-  public void WhenEquallingByEqualsOperator_AndPointsAreSame_ThenPointsShouldBeEqual()
-  {
-    // Arrange.
-    var (firstPoint, secondPoint) = GetSamePoints();
-
-    // Act.
-    var isPointsAreEqual = firstPoint == secondPoint;
-
-    // Assert.
-    isPointsAreEqual.Should().BeTrue();
-  }
-
-  [TestMethod]
-  public void WhenEquallingByEqualsOperator_AndPointsAreDifferent_ThenPointsShouldBeNotEqual()
-  {
-    // Arrange.
-    var (firstPoint, secondPoint) = GetDifferentPoints();
+	[TestMethod]
+	public void WhenCallingDefaultConstructor_ThenXAndYShouldBe0()
+	{
+		// Act.
+		var point = new Point();
+
+		// Assert.
+		point.X.Should().Be(0);
+		point.Y.Should().Be(0);
+	}
+
+	[TestMethod]
+	public void WhenCallingConstructorWithNonZeroNumbers_ThenXAndYShouldBeCorrect()
+	{
+		// Act.
+		var point = new Point(4, 6);
+
+		// Assert.
+		point.X.Should().Be(4);
+		point.Y.Should().Be(6);
+	}
+
+	[TestMethod]
+	public void WhenGettingHashCodes_AndPointsAreSame_ThenHashCodesShouldBeSame()
+	{
+		// Arrange.
+		var (firstPoint, secondPoint) = GetSamePoints();
+
+		// Act.
+		var firstPointHashCode = firstPoint.GetHashCode();
+		var secondPointHashCode = secondPoint.GetHashCode();
+
+		// Assert.
+		firstPointHashCode.Should().Be(secondPointHashCode);
+	}
+
+	[TestMethod]
+	public void WhenEquallingByEqualsMethod_AndPointsAreSame_ThenPointsShouldBeEqual()
+	{
+		// Arrange.
+		var (firstPoint, secondPoint) = GetSamePoints();
+
+		// Act.
+		var isPointsAreEqual = firstPoint.Equals(secondPoint);
+
+		// Assert.
+		isPointsAreEqual.Should().BeTrue();
+	}
+
+	[TestMethod]
+	public void WhenEquallingByEqualsMethod_AndPointsAreDifferent_ThenPointsShouldBeNotEqual()
+	{
+		// Arrange.
+		var (firstPoint, secondPoint) = GetDifferentPoints();
+
+		// Act.
+		var isPointsAreEqual = firstPoint.Equals(secondPoint);
+
+		// Assert.
+		isPointsAreEqual.Should().BeFalse();
+	}
+
+	[TestMethod]
+	public void WhenEquallingByEqualsMethod_AndSecondPointIsNull_ThenPointsShouldBeNotEqual()
+	{
+		// Arrange.
+		var firstPoint = new Point(1, 2);
+
+		// Act.
+		var isPointsAreEqual = firstPoint.Equals(null);
+
+		// Assert.
+		isPointsAreEqual.Should().BeFalse();
+	}
+
+	[TestMethod]
+	public void WhenEquallingByEqualsMethod_AndSecondPointIsInteger_ThenPointsShouldBeNotEqual()
+	{
+		// Arrange.
+		var firstPoint = new Point(1, 2);
+
+		// Act.
+		var isPointsAreEqual = firstPoint.Equals(9);
+
+		// Assert.
+		isPointsAreEqual.Should().BeFalse();
+	}
+
+	[TestMethod]
+	public void WhenEquallingByEqualsOperator_AndPointsAreSame_ThenPointsShouldBeEqual()
+	{
+		// Arrange.
+		var (firstPoint, secondPoint) = GetSamePoints();
+
+		// Act.
+		var isPointsAreEqual = firstPoint == secondPoint;
+
+		// Assert.
+		isPointsAreEqual.Should().BeTrue();
+	}
+
+	[TestMethod]
+	public void WhenEquallingByEqualsOperator_AndPointsAreDifferent_ThenPointsShouldBeNotEqual()
+	{
+		// Arrange.
+		var (firstPoint, secondPoint) = GetDifferentPoints();
 
-    // Act.
-    var isPointsAreEqual = firstPoint == secondPoint;
+		// Act.
+		var isPointsAreEqual = firstPoint == secondPoint;
 
-    // Assert.
-    isPointsAreEqual.Should().BeFalse();
-  }
+		// Assert.
+		isPointsAreEqual.Should().BeFalse();
+	}
 
-  [TestMethod]
-  public void WhenInequallingByNotEqualsOperator_AndPointsAreSame_ThenPointsShouldBeEqual()
-  {
-    // Arrange.
-    var (firstPoint, secondPoint) = GetSamePoints();
+	[TestMethod]
+	public void WhenInequallingByNotEqualsOperator_AndPointsAreSame_ThenPointsShouldBeEqual()
+	{
+		// Arrange.
+		var (firstPoint, secondPoint) = GetSamePoints();
 
-    // Act.
-    var isPointsAreNotEqual = firstPoint != secondPoint;
+		// Act.
+		var isPointsAreNotEqual = firstPoint != secondPoint;
 
-    // Assert.
-    isPointsAreNotEqual.Should().BeFalse();
-  }
+		// Assert.
+		isPointsAreNotEqual.Should().BeFalse();
+	}
 
-  [TestMethod]
-  public void WhenInequallingByNotEqualsOperator_AndPointsAreDifferent_ThenPointsShouldBeNotEqual()
-  {
-    // Arrange.
-    var (firstPoint, secondPoint) = GetDifferentPoints();
+	[TestMethod]
+	public void WhenInequallingByNotEqualsOperator_AndPointsAreDifferent_ThenPointsShouldBeNotEqual()
+	{
+		// Arrange.
+		var (firstPoint, secondPoint) = GetDifferentPoints();
 
-    // Act.
-    var isPointsAreNotEqual = firstPoint != secondPoint;
+		// Act.
+		var isPointsAreNotEqual = firstPoint != secondPoint;
 
-    // Assert.
-    isPointsAreNotEqual.Should().BeTrue();
-  }
+		// Assert.
+		isPointsAreNotEqual.Should().BeTrue();
+	}
 
-  [TestMethod]
-  public void WhenOffsetingPoint_AndOffsetEquals0_ThenOffsetedPointShouldBeEqualToInitialPoint()
-  {
-    // Arrange.
-    var initialPoint = new Point(15, 12);
+	[TestMethod]
+	public void WhenOffsetingPoint_AndOffsetEquals0_ThenOffsetedPointShouldBeEqualToInitialPoint()
+	{
+		// Arrange.
+		var initialPoint = new Point(15, 12);
 
-    var offset = new Point(0, 0);
+		var offset = new Point(0, 0);
 
-    var offsetedPoint = initialPoint.Offset(offset);
+		var offsetedPoint = initialPoint.Offset(offset);
 
-    // Act.
-    var isPointChanged = initialPoint == offsetedPoint;
+		// Act.
+		var isPointChanged = initialPoint == offsetedPoint;
 
-    // Assert.
-    isPointChanged.Should().BeTrue();
-  }
+		// Assert.
+		isPointChanged.Should().BeTrue();
+	}
 
-  [TestMethod]
-  public void WhenOffsetingPoint_AndOffsetPointIsNot0_ThenOffsetedPointShouldBeChanged()
-  {
-    // Arrange.
-    var initialPoint = new Point(6, 2);
+	[TestMethod]
+	public void WhenOffsetingPoint_AndOffsetPointIsNot0_ThenOffsetedPointShouldBeChanged()
+	{
+		// Arrange.
+		var initialPoint = new Point(6, 2);
 
-    var offset = new Point(2, 5);
+		var offset = new Point(2, 5);
 
-    var offsetedPoint = initialPoint.Offset(offset);
+		var offsetedPoint = initialPoint.Offset(offset);
 
-    // Act.
-    var isPointChanged = offsetedPoint.X == 8 && offsetedPoint.Y == 7;
+		// Act.
+		var isPointChanged = offsetedPoint.X == 8 && offsetedPoint.Y == 7;
 
-    // Assert.
-    isPointChanged.Should().BeTrue();
-  }
+		// Assert.
+		isPointChanged.Should().BeTrue();
+	}
 
-  [TestMethod]
-  public void WhenOffsetingPoint_AndOffsetXAndYAreNegative_ThenOffsetedPointShouldBeChanged()
-  {
-    // Arrange.
-    var initialPoint = new Point(5, 5);
+	[TestMethod]
+	public void WhenOffsetingPoint_AndOffsetXAndYAreNegative_ThenOffsetedPointShouldBeChanged()
+	{
+		// Arrange.
+		var initialPoint = new Point(5, 5);
 
-    var offset = new Point(-2, -3);
+		var offset = new Point(-2, -3);
 
-    var offsetedPoint = initialPoint.Offset(offset);
+		var offsetedPoint = initialPoint.Offset(offset);
 
-    // Act.
-    var isPointChanged = offsetedPoint.X == 3 && offsetedPoint.Y == 2;
+		// Act.
+		var isPointChanged = offsetedPoint.X == 3 && offsetedPoint.Y == 2;
 
-    // Assert.
-    isPointChanged.Should().BeTrue();
-  }
+		// Assert.
+		isPointChanged.Should().BeTrue();
+	}
 
-  [TestMethod]
-  public void WhenOffsetingPoint_AndOffsetPointIsNot0_ThenInitialPointNotChanged()
-  {
-    // Arrange.
-    var initialPoint = new Point(2, 3);
+	[TestMethod]
+	public void WhenOffsetingPoint_AndOffsetPointIsNot0_ThenInitialPointNotChanged()
+	{
+		// Arrange.
+		var initialPoint = new Point(2, 3);
 
-    var offset = new Point(2, 4);
+		var offset = new Point(2, 4);
 
-    initialPoint.Offset(offset);
+		initialPoint.Offset(offset);
 
-    // Act.
-    var isPointChanged = initialPoint == new Point(2, 3);
+		// Act.
+		var isPointChanged = initialPoint == new Point(2, 3);
 
-    // Assert.
-    isPointChanged.Should().BeTrue();
-  }
+		// Assert.
+		isPointChanged.Should().BeTrue();
+	}
 
-  [TestMethod]
-  public void WhenSubtracting_AndPontsAreTheSame_ThenPointXShouldBe0AndPointYShouldBe0()
-  {
-    // Arrange.
-    var (firstPoint, secondPoint) = GetSamePoints();
+	[TestMethod]
+	public void WhenSubtracting_AndPontsAreTheSame_ThenPointXShouldBe0AndPointYShouldBe0()
+	{
+		// Arrange.
+		var (firstPoint, secondPoint) = GetSamePoints();
 
-    // Act.
-    var result = firstPoint - secondPoint;
+		// Act.
+		var result = firstPoint - secondPoint;
 
-    // Assert.s
-    result.Should().Be(new Point(0, 0));
-  }
+		// Assert.s
+		result.Should().Be(new Point(0, 0));
+	}
 
 
-  [TestMethod]
-  public void WhenSubtracting_AndFirstPointXIsPositiveAndYIsNegativeAndSecondPointXNegativeAndYPositive_ThenPointShouldBeCorrect()
-  {
-    // Arrange.
-    var firstPoint = new Point(7, -9);
-    var secondPoint = new Point(-5, 4);
+	[TestMethod]
+	public void WhenSubtracting_AndFirstPointXIsPositiveAndYIsNegativeAndSecondPointXNegativeAndYPositive_ThenPointShouldBeCorrect()
+	{
+		// Arrange.
+		var firstPoint = new Point(7, -9);
+		var secondPoint = new Point(-5, 4);
 
-    // Act.
-    var result = firstPoint - secondPoint;
+		// Act.
+		var result = firstPoint - secondPoint;
 
-    // Assert.
-    result.Should().Be(new Point(12, -13));
-  }
+		// Assert.
+		result.Should().Be(new Point(12, -13));
+	}
 
-  [TestMethod]
-  public void WhenSubtracting_AndPontsAre0_ThenPointXShouldBe0AndPointYShouldBe0()
-  {
-    // Arrange.
-    var firstPoint = new Point(0, 0);
-    var secondPoint = new Point(0, 0);
+	[TestMethod]
+	public void WhenSubtracting_AndPontsAre0_ThenPointXShouldBe0AndPointYShouldBe0()
+	{
+		// Arrange.
+		var firstPoint = new Point(0, 0);
+		var secondPoint = new Point(0, 0);
 
-    // Act.
-    var result = firstPoint - secondPoint;
+		// Act.
+		var result = firstPoint - secondPoint;
 
-    // Assert.
-    result.Should().Be(new Point(0, 0));
-  }
+		// Assert.
+		result.Should().Be(new Point(0, 0));
+	}
 
-  private static (Point firstPoint, Point secondPoint) GetSamePoints()
-  {
-    const int x = 45;
-    const int y = 24;
+	private static (Point firstPoint, Point secondPoint) GetSamePoints()
+	{
+		const int x = 45;
+		const int y = 24;
 
-    var firstPoint = new Point(x, y);
-    var secondPoint = new Point(x, y);
+		var firstPoint = new Point(x, y);
+		var secondPoint = new Point(x, y);
 
-    return (firstPoint, secondPoint);
-  }
+		return (firstPoint, secondPoint);
+	}
 
-  private static (Point firstPoint, Point secondPoint) GetDifferentPoints()
-  {
-    var firstPoint = new Point(1, 3);
-    var secondPoint = new Point(2, 7);
+	private static (Point firstPoint, Point secondPoint) GetDifferentPoints()
+	{
+		var firstPoint = new Point(1, 3);
+		var secondPoint = new Point(2, 7);
 
-    return (firstPoint, secondPoint);
-  }
+		return (firstPoint, secondPoint);
+	}
 }
