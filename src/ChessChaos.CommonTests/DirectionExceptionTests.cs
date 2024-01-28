4d40a75d3474fb01b434327a01f4fb047a6aa419
Kirill <sosisks888@mail.ru>
2024-01-28 13:38:04 +0300

    dividing test cases into separate classes

---
+++ src/ChessChaos.CommonTests/DirectionExceptionTests.cs
@@ -0,0 +1,99 @@
+﻿
+using ChessChaos.Common;
+using FluentAssertions;
+
+namespace ChessChaos.CommonTests;
+
+[TestClass]
+public class DirectionExceptionTests
+{
+	[TestMethod]
+	public void WhenDirectingPoints_AndPointsSourceAndTargetAre0_ThenThrowArgumentException()
+	{
+		// Arange.
+		var source = new Point(0, 0);
+		var target = new Point(0, 0);
+		var resultPoint = new Point(0, 0);
+
+		// Act.
+		Action act = () => resultPoint.GetNormalizationVectors(source, target);
+
+		// Assert.
+		act.Should().Throw<ArgumentException>();
+	}
+
+	[TestMethod]
+	public void WhenDirectingPoints_AndPointsSourceAndTargetAreTheSame_ThenThrowArgumentException()
+	{
+		// Arange.
+		var source = new Point(4, 4);
+		var target = new Point(4, 4);
+		var resultPoint = new Point();
+
+		// Act.
+		Action act = () => resultPoint.GetNormalizationVectors(source, target);
+
+		// Assert.
+		act.Should().Throw<ArgumentException>();
+	}
+
+	[TestMethod]
+	public void WhenDirectingPoints_AndPointsTargetXEqualsSourceX_ThenThrowException()
+	{
+		// Arange.
+		var source = new Point(4, 0);
+		var target = new Point(4, 0);
+		var resultPoint = new Point();
+
+		// Act.
+		Action act = () => resultPoint.GetNormalizationVectors(source, target);
+
+		// Assert.
+		act.Should().Throw<DivideByZeroException>();
+	}
+
+	[TestMethod]
+	public void WhenDirectingPoints_AndPointsTargetYEqualsSourceY_ThenThrowException()
+	{
+		// Arange.
+		var source = new Point(0, 7);
+		var target = new Point(0, 7);
+		var resultPoint = new Point();
+
+		// Act.
+		Action act = () => resultPoint.GetNormalizationVectors(source, target);
+
+		// Assert.
+		act.Should().Throw<DivideByZeroException>();
+	}
+
+	[TestMethod]
+	public void WhenDirectingPoints_AndPointsTargetXEqualsSourceXAndTargetYNotEqualsSourceY_ThenThrowException()
+	{
+		// Arange.
+		var source = new Point(8, 7);
+		var target = new Point(8, 9);
+		var resultPoint = new Point();
+
+		// Act.
+		Action act = () => resultPoint.GetNormalizationVectors(source, target);
+
+		// Assert.
+		act.Should().Throw<DivideByZeroException>();
+	}
+
+	[TestMethod]
+	public void WhenDirectingPoints_AndPointsTargetYEqualsSourceXAndTargetXNotEqualsSourceX_ThenThrowException()
+	{
+		// Arange.
+		var source = new Point(8, 3);
+		var target = new Point(4, 3);
+		var resultPoint = new Point();
+
+		// Act.
+		Action act = () => resultPoint.GetNormalizationVectors(source, target);
+
+		// Assert.
+		act.Should().Throw<DivideByZeroException>();
+	}
+}
---
+++ src/ChessChaos.CommonTests/EqualsPointOperatorTests.cs
@@ -0,0 +1,53 @@
+﻿using ChessChaos.Common;
+using FluentAssertions;
+
+namespace ChessChaos.CommonTests;
+
+[TestClass]
+public class EqualsPointOperatorTests
+{
+	[TestMethod]
+	public void WhenEquallingByEqualsOperator_AndPointsAreSame_ThenPointsShouldBeEqual()
+	{
+		// Arrange.
+		var (firstPoint, secondPoint) = GetSamePoints();
+
+		// Act.
+		var isPointsAreEqual = firstPoint == secondPoint;
+
+		// Assert.
+		isPointsAreEqual.Should().BeTrue();
+	}
+
+	[TestMethod]
+	public void WhenEquallingByEqualsOperator_AndPointsAreDifferent_ThenPointsShouldBeNotEqual()
+	{
+		// Arrange.
+		var (firstPoint, secondPoint) = GetDifferentPoints();
+
+		// Act.
+		var isPointsAreEqual = firstPoint == secondPoint;
+
+		// Assert.
+		isPointsAreEqual.Should().BeFalse();
+	}
+
+	private static (Point firstPoint, Point secondPoint) GetSamePoints()
+	{
+		const int x = 45;
+		const int y = 24;
+
+		var firstPoint = new Point(x, y);
+		var secondPoint = new Point(x, y);
+
+		return (firstPoint, secondPoint);
+	}
+
+	private static (Point firstPoint, Point secondPoint) GetDifferentPoints()
+	{
+		var firstPoint = new Point(1, 3);
+		var secondPoint = new Point(2, 7);
+
+		return (firstPoint, secondPoint);
+	}
+}
