9ab56dc8bc4374d6e12cae67c5bd0cc9fb0fdd65
NetMonster <sosisks888@mail.ru>
2024-02-08 02:33:25 -0800

    Add DirectionPoint method and also add tests for this method (#22)
    * Add DirectionPoint method and also add tests for this method
    
    * fix GetDirection method
    
    * add a method that calculates direction horizontally and vertically
    
    * refactoring code and fixing spelling errors
    
    * dividing test cases into separate classes
    
    * refactoring code
    
    * refactoring tests code
    
    * refactoring Point.cs and TestPoints.cs code
    
    * refactoring tests code
    
    * refactoring code in class Point
    
    * fix algorithm direction points
    
    * fix tests cases
    
    * fix point direct algorithm
    
    * fix code tests with DynamicData
    
    * refactoring code in Point and PointDirectionTests
    
    * refactoring code
    
    * fix GetDirection method
    
    * refactoring code
    
    * fix

--- src/ChessChaos.Common/Point.cs
+++ src/ChessChaos.Common/Point.cs
@@ -33,4 +33,62 @@ public readonly struct Point
 
 	public static Point operator -(Point p1, Point p2)
 	  => new Point(p1.X - p2.X, p1.Y - p2.Y);
+
+	public static Point GetDirection(Point from, Point to)
+	{
+		ThrowIfNotValid(from, to);
+
+		return IsDiagonalDirection(from, to)
+			? GetDiagonalDirection(from, to)
+			: GetHorizontalOrVerticalDirection(from, to);
+	}
+
+	private static Point GetHorizontalOrVerticalDirection(Point from, Point to)
+	{
+		return from.X == to.X
+			? GetVerticalDirection(from, to)
+			: GetHorizontalDirection(from, to);
+	}
+
+	private static Point GetVerticalDirection(Point from, Point to)
+	{
+		return from.Y > to.Y
+			? new Point(0, -1)
+			: new Point(0, 1);
+	}
+
+	private static Point GetHorizontalDirection(Point from, Point to)
+	{
+		return from.X > to.X
+			? new Point(-1, 0)
+			: new Point(1, 0);
+	}
+
+	private static Point GetDiagonalDirection(Point from, Point to)
+	{
+		var differenceBetweenX = to.X - from.X;
+		var differenceBetweenY = to.Y - from.Y;
+
+		return new Point(differenceBetweenX / Math.Abs(differenceBetweenX),
+				differenceBetweenY / Math.Abs(differenceBetweenY));
+	}
+
+	private static void ThrowIfNotValid(Point from, Point to)
+	{
+		if ((from.X == to.X && from.Y == to.Y)
+			|| (from.X != to.X && from.Y != to.Y
+			&& (Math.Abs(Math.Abs(from.X) - Math.Abs(to.X))
+			!= Math.Abs(Math.Abs(from.Y) - Math.Abs(to.Y)))))
+		{
+			throw new ArgumentException("The direction is not valid");
+		}
+	}
+
+	private static bool IsDiagonalDirection(Point from, Point to)
+	{
+		var differenceBetweenX = Math.Abs(from.X) - Math.Abs(to.X);
+		var differenceBetweenY = Math.Abs(from.Y) - Math.Abs(to.Y);
+
+		return Math.Abs(differenceBetweenX) == Math.Abs(differenceBetweenY);
+	}
 }
